174302cf7b3140fa342cbffb17c8981794c1725e
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-08-22 17:04:49 +0300

    Define ConfigService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
 
 @Injectable()
 export class AppService {
+  constructor(config: ConfigService) {
+    console.log(config.get('DB_USERNAME'))
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
