ab5006db832986d79781cfbcafba9708c3379f81
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-08-22 17:12:20 +0300

    Validate env config

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,11 +1,34 @@
 import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
 import * as fs from 'fs';
+import {EnvConfig} from './config.types';
 
 export class ConfigService {
   private readonly envConfig: { [key: string]: string };
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath))
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    console.log(envConfig)
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      DB_HOSTNAME: Joi.string().required(),
+      DB_PORT: Joi.number().required(),
+      DB_USERNAME: Joi.string().required(),
+      DB_PASSWORD: Joi.string().required(),
+      DB_NAME: Joi.string().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
   }
 
   get(key: string): string {
