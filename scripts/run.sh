c66699a2cc48ccb42404bfd6389df742a411b995
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-09-03 12:34:04 +0300

    Update bash/run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name runtime -e "NODE_ENV=local" -e "DB_HOSTNAME=host.docker.internal" -p 8080:3000 nestjs-boilerplate:latest
+docker run -d --rm --name runtime -e "NODE_ENV=local" -e "DB_HOSTNAME=host.docker.internal" -e "DB_NAME=auth-db" -p 8080:3000 nestjs-boilerplate:latest
